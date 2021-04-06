d647983a709f9342ab98d7944869fc586b339aae
croftik <70719390+croftik@users.noreply.github.com>
2021-04-06 13:03:45 +0300

    refactor: rename selectors and delete class of element

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById("article_new-task");//Add a new task.
+var taskInput=document.getElementById("form__input");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
 var incompleteTaskHolder=document.getElementById("article_todo");//ul of #incompleteTasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
@@ -18,7 +18,6 @@ var completedTasksHolder=document.getElementById("completed-tasks");//completed-
 var createNewTaskElement=function(taskString){
 
     var listItem=document.createElement("section");
-    listItem.classList.add("section");
     //input (checkbox)
     var checkBox=document.createElement("input");//checkbx
     //label
